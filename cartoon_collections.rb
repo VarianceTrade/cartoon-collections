b097d9f50e9480faef65644c0bc410a8ae086e28
Brandon <sugb73@gmail.com>
2021-01-21 11:45:16 -0500

    commit tree 5848a5aa4eda4af3769c9ce54bd302d088b2348a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.map.with_index(1) do |name, index|
+    puts "#{index} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+
+def summon_captain_planet(planeteers)# code an argument here
   # Your code here
+    planeteers.map do |planeteer|
+      "#{planeteer.capitalize}!"
+    end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+    array.any? {|new| new.length > 4}
+
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+# [1,2,3].include? 2
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |snack|
+    if cheese_types.include? snack
+      return snack
+    end
+  end
+
+  return nil
 end
